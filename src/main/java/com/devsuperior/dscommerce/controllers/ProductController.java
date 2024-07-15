ec9377e1e2ee8b6a73a82d8c440824aba1e151a6
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-07-14 22:01:22 -0300

    get products page

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 //controller implements the resources
 //controller that respond on route '/products'
 @RestController
-//endpoint to respond on route '/products'
 @RequestMapping(value = "/products")
 public class ProductController {
 
@@ -19,10 +20,20 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    //method for return product dto
-    @GetMapping(value = "/{id}") //this '/{id}' is for received several parameter on the route
-    public ProductDTO findById(@PathVariable Long id) { //pathvariable received several id on the route //same method in class ProductService
-         ProductDTO dto = service.findById(id);
+    //method for return product dto.
+    //this '/{id}' is for received several parameter on the route
+    //pathvariable received several id on the route
+    //findById() is the same method in class ProductService
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    //pageable to view products by pages
+    //findAll() is the same method in class ProductService
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,11 +3,13 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
-//is here, go implement the search on database
+//implement the search on database, with dependency ProductRepository
 @Service
 public class ProductService {
 
@@ -15,13 +17,24 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
-    //method
-    @Transactional(readOnly = true) //only ready operation in database(dont lock in database)
+    //findById method to search products on database for id
+    //only ready operation in database(don't lock in database)
+    //first search product through id, after handle this product and save one variable, after convert this product in one product dto.
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Optional<Product> result = repository.findById(id); //search on database the product with method findById()
-        Product product = result.get(); //get the product
-        ProductDTO dto = new ProductDTO(product); //convert product for one product dto
-        return dto;
+            Optional<Product> result = repository.findById(id);
+            Product product = result.get();
+            ProductDTO dto = new ProductDTO(product);
+            return dto;
     }
 
+    //findAll method to search all products on database
+    //first search all products, after put them pageable and save on Page list, and after transform this list in one list ProductDTO.
+    //obs. page is a stream. only user map() in this case, don't need use stream() here.
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+            Page<Product> result = repository.findAll(pageable);
+            return result.map(x -> new ProductDTO(x));
+
+    }
 }
